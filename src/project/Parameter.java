9039cea9a74845d791d7544b52cb54be6b06e78b
anne <Anne.Holmen@HjemMedStil.no>
2021-08-13 14:59:02 +0200

    Parameter arver fra Part, og legger til en data_type

--- src/project/Parameter.java
+++ src/project/Parameter.java
@@ -1,58 +1,19 @@
 package project;
 
-/**
- * Input-parameter to a Method
- */
-public class Parameter {
-    private String name;
-    private String type;
-    private String description;
-
-    /**
-     * Gets the name of the parameter
-     * @return String
-     */
-    String getName(){
-        return this.name;
+public class Parameter extends Part {
+    String data_type;
+    Parameter(String type){
+        super(type);
+    }
+    Parameter(String name, String data_type){
+        super("parameter", name);
+        this.data_type = data_type;
+    }
+    void setDataType(String data_type){
+        this.data_type = data_type;
     }
 
-    /**
-     * Sets the name of the parameter
-     * @param name String
-     */
-    void setName(String name){
-        this.name = name;
-    }
-
-    /**
-     * Gets the type of the parameter
-     * @return String
-     */
-    String getType(){
-        return this.type;
-    }
-
-    /**
-     * Sets the type of the parameter
-     * @param type String
-     */
-    void setType(String type){
-        this.type = type;
-    }
-
-    /**
-     * Gets the description of the parameter
-     * @return String
-     */
-    String getDescription(){
-        return this.description;
-    }
-
-    /**
-     * Sets the description of the parameter
-     * @param description String
-     */
-    void setDescription(String description){
-        this.description = description;
+    String getDataType(){
+        return this.data_type;
     }
 }
